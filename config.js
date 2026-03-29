99a8dd3c2faf2583561290100d9e8013fa1344cc
borgerobertsen <borge.robertsen@visma.com>
2026-03-29 10:18:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/a/macros/visma.com/s/AKfycbwOTHza0OcNI0G_fyuTRkWHT8sG2fof4nzqBnCtdbsLSDKHxlwyKzwKPIMucMSm2pNA/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx-qiF-Xah_nsKachyvcEO5BKoOuO2ahb6cbjFHHpl6QpipLLFyr3zpxjBmeM8-vaml/exec",
   HUBSPOT_TOKEN: "FYLL_INN_PRIVATE_APP_TOKEN_HER",
   HUBSPOT_PORTAL_ID: "FYLL_INN_PORTAL_ID_HER"
 };
